88d358952c4cb8af844450da2bb674bd8b1036d4
salah224 <salah224@hotmail.com>
2019-04-07 09:23:13 -0500

    ifA

---
+++ IF/src/IF.java
@@ -0,0 +1,25 @@
+
+public class IF {
+
+	public IF() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		int num = 7;
+ if(num == 8){
+	 System.out.println("the number is equal to 8");
+ }
+ if(num > 8){
+	 System.out.println("the number is greater than 8");
+ }
+ if(num < 8){
+	 System.out.println("the number is less than 8");
+ if(num <= 8){
+	 System.out.println("the number less or quale to 8");
+ }
+ }
+	}
+
+}
